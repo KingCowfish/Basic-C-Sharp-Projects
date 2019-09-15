90284011add2910f4cc052394d1e70d1c4eb1482
Alexander Broder <brodera146@gmail.com>
2019-09-14 18:38:01 -0700

    Add assignment.

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number that you wish me to use my magical methods on.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Math stats = new Math();
+            Console.WriteLine(stats.Division(num1));
+            Console.WriteLine(stats.Multiply(num1));
+            Console.WriteLine(stats.Addition(num1));
+            Console.ReadLine();
+        }
+    }
+}
