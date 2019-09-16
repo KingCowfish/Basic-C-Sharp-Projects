370a985ff37716653890a1b8ab3bc687b9dac82f
Alexander Broder <brodera146@gmail.com>
2019-09-16 10:56:03 -0700

    Add assignment.

---
+++ Methods3/Methods3/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Math
+    {
+        public int Mathing(int num1)
+        {
+            int mult = num1 * 11;
+            return mult;
+        }
+
+        public decimal Mathing(decimal num1)
+        {
+            decimal c = num1 - 362;
+            return c;
+        }
+
+        public string Mathing(string num1)
+        {
+            int g = Convert.ToInt32(num1);
+            int h = g + 27;
+            return h.ToString();
+        }
+    }
+}
