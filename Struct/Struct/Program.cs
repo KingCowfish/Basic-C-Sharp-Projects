e252ba52dd631ca4a23834164800f54d1f835b5e
Alexander Broder <brodera146@gmail.com>
2019-09-19 12:23:22 -0700

    Add assignment.

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 75;
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
